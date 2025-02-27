46537ac6a6d3c296722bea68a4f12a6e9c12e115
Aurélien PIERRE <aurelien@aurelienpierre.com>
2025-02-27 02:12:00 +0100

    Comment and document shit

--- src/develop/develop.h
+++ src/develop/develop.h
@@ -488,7 +488,11 @@ uint64_t dt_dev_hash(dt_develop_t *dev, struct dt_dev_pixelpipe_t *pipe);;
 /** wait until hash value found in hash matches hash value defined by dev/pipe/pmin/pmax with timeout */
 int dt_dev_wait_hash(dt_develop_t *dev, struct dt_dev_pixelpipe_t *pipe, const double iop_order, const int transf_direction, dt_pthread_mutex_t *lock,
                      const volatile uint64_t *const hash);
-/** synchronize pixelpipe by means hash values by waiting with timeout and potential reprocessing */
+/** synchronize pixelpipe by means hash values by waiting with timeout and potential reprocessing
+ * FIXME: modules that need to resync internal data with pipeline should listen to the PREVIEW_PIPE_RECOMPUTED signal.
+ * This function relies on timeouts, waiting for the pipe to finish, and the exception is not caught if the
+ * timeout expires with no result.
+*/
 int dt_dev_sync_pixelpipe_hash(dt_develop_t *dev, struct dt_dev_pixelpipe_t *pipe, const double iop_order, const int transf_direction, dt_pthread_mutex_t *lock,
                                const volatile uint64_t *const hash);
 
