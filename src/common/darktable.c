25362d434b5aadcb29ba4db6feb2e519813f6b24
Aurélien PIERRE <aurelien@aurelienpierre.com>
2025-03-26 16:25:42 +0100

    Revert/reorder --debug "fix"

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -578,11 +578,6 @@ int dt_init(int argc, char *argv[], const gboolean init_gui, const gboolean load
         argv[k-1] = NULL;
         argv[k] = NULL;
       }
-      else if(!strcmp(argv[k], "--debug"))
-      {
-        k++;
-        argv[k] = NULL;
-      }
       else if(!strcmp(argv[k], "--moduledir") && argc > k + 1)
       {
         moduledir_from_command = argv[++k];
@@ -810,6 +805,10 @@ int dt_init(int argc, char *argv[], const gboolean init_gui, const gboolean load
 #ifdef HAVE_OPENCL
         exclude_opencl = TRUE;
 #endif
+        argv[k] = NULL;
+      }
+      else if(!strcmp(argv[k], "--debug"))
+      {
         argv[k] = NULL;
       }
       else if(!strcmp(argv[k], "--"))
